578cddc25ffe763ca0c32a1b6f14a764a6dcd31e
mhlee80 <myeonghyeon.lee@gmail.com>
2020-06-23 16:08:36 +0900

    NotoSans 추가

--- Fonts/ViewController.swift
+++ Fonts/ViewController.swift
@@ -43,7 +43,7 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 3
+    return 10
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -63,6 +63,34 @@ extension ViewController: UITableViewDataSource {
       cell.textLabel?.font = UIFont(name: "NotoSansCJKkr-Regular", size: 30)
       cell.textLabel?.numberOfLines = 2
       cell.textLabel?.text = "Noto Sans\n안녕하세요 Hello 12345"
+    case 3:
+      cell.textLabel?.font = UIFont(name: "Lato-Bold", size: 30)
+      cell.textLabel?.numberOfLines = 2
+      cell.textLabel?.text = "Lato Bold\n안녕하세요 Hello 12345"
+    case 4:
+      cell.textLabel?.font = UIFont(name: "NotoSansKR-Black", size: 30)
+      cell.textLabel?.numberOfLines = 2
+      cell.textLabel?.text = "NotoSansKR-Black\n안녕하세요 Hello 12345"
+    case 5:
+      cell.textLabel?.font = UIFont(name: "NotoSansKR-Bold", size: 30)
+      cell.textLabel?.numberOfLines = 2
+      cell.textLabel?.text = "NotoSansKR-Bold\n안녕하세요 Hello 12345"
+    case 6:
+      cell.textLabel?.font = UIFont(name: "NotoSansKR-Light", size: 30)
+      cell.textLabel?.numberOfLines = 2
+      cell.textLabel?.text = "NotoSansKR-Light\n안녕하세요 Hello 12345"
+    case 7:
+      cell.textLabel?.font = UIFont(name: "NotoSansKR-Medium", size: 30)
+      cell.textLabel?.numberOfLines = 2
+      cell.textLabel?.text = "NotoSansKR-Medium\n안녕하세요 Hello 12345"
+    case 8:
+      cell.textLabel?.font = UIFont(name: "NotoSansKR-Regular", size: 30)
+      cell.textLabel?.numberOfLines = 2
+      cell.textLabel?.text = "NotoSansKR-Regular\n안녕하세요 Hello 12345"
+    case 9:
+      cell.textLabel?.font = UIFont(name: "NotoSansKR-Thin", size: 30)
+      cell.textLabel?.numberOfLines = 2
+      cell.textLabel?.text = "NotoSansKR-Thin\n안녕하세요 Hello 12345"
       
     default:
       break
@@ -71,6 +99,13 @@ extension ViewController: UITableViewDataSource {
     return cell
   }
 }
+
+//== NotoSansKR-Bold
+//== NotoSansKR-Thin
+//== NotoSansKR-Black
+//== NotoSansKR-Light
+//== NotoSansKR-Medium
+//== NotoSansKR-Regular
 
 extension ViewController: UITableViewDelegate {
   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
